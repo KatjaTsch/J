8ba03d28bea7d9ba9759d668cd44224536a86061
KatjaTsch <katjatschebotar.er@mail.ru>
2023-04-09 14:41:05 +0300

    pr_002_1.java

--- pr_002_1.java
+++ pr_002_1.java
@@ -10,23 +10,55 @@ public class pr_002_1 {
     
     public static void main(String[] args) {
         
-        String jsonString = {"фамилия":"Иванов","оценка":"5","предмет":"Математика"}, {"фамилия":"Петрова","оценка":"4","предмет":"Информатика"}, {"фамилия":"Краснов","оценка":"5","предмет":"Физика"}};
-        System.out.println(ex3(jsonString));
+        String jsString = "[{\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}," +
+        "{\"фамилия\":\"Петрова\",\"оценка\":\"4\",\"предмет\":\"Информатика\"}," +
+        "{\"фамилия\":\"Краснов\",\"оценка\":\"5\",\"предмет\":\"Физика\"}]";
+        parsejsonString(jsString);
+                
+        
+        }
+        
+        public static void parsejsonString (String jsString){
+        StringBuilder stringBuilder = new StringBuilder();
+        jsString = jsString.substring(1, jsString.length() - 1);
+        String[] objects = jsString.split("},");
+        
+        
+        for(String object :objects){
+            object = object.replace("{", "").replace("}", "");
+            String[] res = object.split(",");
+
+            String surname = res[0].split(":")[1].replace("\"", " ");
+            String grade = res[1].split(":")[1].replace("\"", " ");
+            String less = res[2].split(":")[1].replace("\"", " ");
+
+            stringBuilder.append("Студент").append(surname).append("получил").append(grade).append("по предмету").append(less).append("\n");
+        }
+        System.out.println(stringBuilder);
+
+
+
+        // String [] temp = jsString.split("},");
+        // String res = "";
+        // for (int i = temp.length-1; i >= 0; i--) {
+        // res += temp[i] + " ";{
+
+        // }
+
+    //         String [] temp = jsString.split(",");
+    //         String res = "";
+    //        for (int i = temp.length-1; i >= 0; i--) {
+    //        res += temp[i] + " ";
+    //        String output= res.replace(';',' ');
+    // System.out.println(output);
+
+            }
+            
+        }
+           
+
+                     
+        
     
-    static String ex3(String jsonString){
-        String [] temp = jsonString.split(" ");
-        String res = "";
-        for (int i = temp.length - 1; i >= 0; i--) {
-            res += temp[i] + " ";
-        }
-        return res;
-        }
-        // StringBuilder stringBuilder = new StringBuilder();
-        // for (int i = 0; i < temp.length - 1; i++){
-        //     stringBuilder.append(res);
-             
-        }
-        return stringBuilder.toString();
-    }
     
     
