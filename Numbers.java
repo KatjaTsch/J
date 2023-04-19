a86171ebfe85cc28191c13bcd6d16fba5571e911
KatjaTsch <katjatschebotar.er@mail.ru>
2023-04-19 19:22:56 +0300

    numbers

---
+++ Numbers.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Numbers {
+    Map<String, String> map = new HashMap<>();
+
+    void add(String passport, String name) {
+        map.put(passport, name);
+    }
+
+    void getByName(String name) {
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (entry.getValue().equals(name)) {
+                System.out.println(entry);
+            }
+        }
+    }
+}
+
